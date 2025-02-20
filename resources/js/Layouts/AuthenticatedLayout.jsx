ac396f53d8996fc47c1e5667c92c673988b872fb
Jia Michelle Jimenea <jiamichellejimenea@gmail.com>
2025-02-20 15:41:23 +0800

    Create Course.php
    Course Module

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -31,6 +31,15 @@ export default function AuthenticatedLayout({ header, children }) {
                                     Dashboard
                                 </NavLink>
                             </div>
+
+                            <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                                <NavLink
+                                    href={route('course.index')}
+                                    active={route().current('course.index')}
+                                >
+                                    Course
+                                </NavLink>
+                            </div>
                         </div>
 
                         <div className="hidden sm:ms-6 sm:flex sm:items-center">
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CourseController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -22,6 +23,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// Route for displaying the courses page
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/courses', [CourseController::class, 'index'])->name('course.index');
+    Route::get('/courses/create', [CourseController::class, 'create'])->name('course.create');
+    Route::post('/courses', [CourseController::class, 'store'])->name('course.store');
+    Route::get('/courses/edit/{id}', [CourseController::class, 'edit'])->name('course.edit');
+    Route::put('/courses/{id}', [CourseController::class, 'update'])->name('course.update');
+    Route::delete('/courses/{id}', [CourseController::class, 'destroy'])->name('course.destroy');
 });
 
 require __DIR__.'/auth.php';
